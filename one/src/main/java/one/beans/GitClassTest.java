fa5f17dd5b79d51f4175221f4fdc8c04069ff2ac
reinty <tibg52.0@qq.com>
2017-07-02 23:00:21 +0800

    just commit

---
+++ one/src/main/java/one/beans/GitClassTest.java
@@ -0,0 +1,25 @@
+package one.beans;
+
+/**
+ * Created by tibg5 on 2017/7/2.
+ */
+public class GitClassTest {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
